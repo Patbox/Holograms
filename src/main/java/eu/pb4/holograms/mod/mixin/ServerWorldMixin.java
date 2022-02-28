a90e62504209b3dac7d8ba4c0e07e54cd13d10f0
Patbox <39821509+Patbox@users.noreply.github.com>
2022-02-28 16:07:51 +0100

    Update to 1.18.2

--- src/main/java/eu/pb4/holograms/mod/mixin/ServerWorldMixin.java
+++ src/main/java/eu/pb4/holograms/mod/mixin/ServerWorldMixin.java
@@ -5,11 +5,11 @@ import eu.pb4.holograms.mod.hologram.HologramManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.WorldGenerationProgressListener;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.PersistentStateManager;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
-import net.minecraft.world.gen.Spawner;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.level.ServerWorldProperties;
 import net.minecraft.world.level.storage.LevelStorage;
@@ -30,7 +30,7 @@ public abstract class ServerWorldMixin implements HoloServerWorld {
     @Shadow public abstract PersistentStateManager getPersistentStateManager();
 
     @Inject(method = "<init>", at = @At("TAIL"))
-    private void initiateHolograms(MinecraftServer server, Executor workerExecutor, LevelStorage.Session session, ServerWorldProperties properties, RegistryKey<World> worldKey, DimensionType dimensionType, WorldGenerationProgressListener worldGenerationProgressListener, ChunkGenerator chunkGenerator, boolean debugWorld, long seed, List<Spawner> spawners, boolean shouldTickTime, CallbackInfo ci) {
+    private void initiateHolograms(MinecraftServer server, Executor workerExecutor, LevelStorage.Session session, ServerWorldProperties properties, RegistryKey worldKey, RegistryEntry registryEntry, WorldGenerationProgressListener worldGenerationProgressListener, ChunkGenerator chunkGenerator, boolean debugWorld, long seed, List spawners, boolean shouldTickTime, CallbackInfo ci) {
         this.hologramManager = this.getPersistentStateManager().getOrCreate((nbtCompound) -> HologramManager.fromNbt((ServerWorld) (Object) this, nbtCompound),
                 () -> new HologramManager((ServerWorld) (Object) this),
         "holograms");
